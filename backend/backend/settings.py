1cd0fc1b5994893bc13f32722c02daca4bc74a90
Pratham Bhoir <pratham.bhoir@buyceps.com>
2023-05-21 01:08:28 +0530

    changes for railway

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,9 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'    #/path/to/static_root
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')    #/path/to/static_root
 MEDIA_URL = 'media/'
-MEDIA_ROOT = 'media/'    #/path/to/media_root
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')   #/path/to/media_root
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
